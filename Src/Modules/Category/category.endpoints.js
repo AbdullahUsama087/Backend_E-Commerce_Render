4c3142a028923a64e85ea020e1000029f3485e54
AbullahUsama087 <abdullah.usama087@gmail.com>
2024-04-14 16:17:08 +0200

    Category EndPoints to Authorize

---
+++ Src/Modules/Category/category.endpoints.js
@@ -0,0 +1,7 @@
+import systemRoles from "../../Utils/systemRoles.js";
+
+const categoryApisRoles = {
+  CATEGORY_ROLES: [systemRoles.ADMIN, systemRoles.SUPER_ADMIN],
+};
+
+export default categoryApisRoles;
