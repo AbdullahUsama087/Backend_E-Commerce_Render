741b225a760199302853894d94eb16ee8d47e508
AbullahUsama087 <abdullah.usama087@gmail.com>
2024-04-14 16:39:56 +0200

    Allowed Extensions that user can upload

---
+++ Src/Utils/allowedExtensions.js
@@ -0,0 +1,7 @@
+const allowedExtensions = {
+  Image: ["image/jpeg", "image/png", "image/webp", "image/gif"],
+  Video: ["video/mpeg", "video/mp4", "video/mkv"],
+  Files: ["application/pdf"]
+};
+
+export default allowedExtensions;
