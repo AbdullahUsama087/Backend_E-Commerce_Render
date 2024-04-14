280d0b3d55f8b318029700c3969420ba7cf2671e
AbullahUsama087 <abdullah.usama087@gmail.com>
2024-04-14 16:29:27 +0200

    Controllers and Routes (APIs) for All Modules

---
+++ Src/Modules/index.routes.js
@@ -0,0 +1,29 @@
+import categoryRouter from "./Category/category.routes.js";
+
+import subCategoryRouter from "./SubCateogory/subCategory.routes.js";
+
+import brandRouter from "./Brand/brand.routes.js";
+
+import productRouter from "./Product/product.routes.js";
+
+import couponRouter from "./Coupon/coupon.routes.js";
+
+import cartRouter from "./Cart/cart.routes.js";
+
+import orderRouter from "./Order/order.routes.js";
+
+import reviewRouter from "./Review/review.routes.js";
+
+import authRouter from "./Auth/auth.routes.js";
+
+export {
+  categoryRouter,
+  subCategoryRouter,
+  brandRouter,
+  productRouter,
+  couponRouter,
+  cartRouter,
+  orderRouter,
+  reviewRouter,
+  authRouter,
+};
