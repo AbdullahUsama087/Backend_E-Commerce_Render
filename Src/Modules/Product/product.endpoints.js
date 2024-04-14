1c8531ca6cffafba67cb54ed0160376fbd8f2374
AbullahUsama087 <abdullah.usama087@gmail.com>
2024-04-14 16:22:24 +0200

    Product EndPoints for Authorization

---
+++ Src/Modules/Product/product.endpoints.js
@@ -0,0 +1,6 @@
+import systemRoles from "../../Utils/systemRoles.js";
+
+const productApisRoles = {
+  PRODUCT_ROLES: [systemRoles.ADMIN, systemRoles.SUPER_ADMIN],
+};
+export default productApisRoles;
