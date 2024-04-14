27f98ca50f2bc8089cc0a9243ed71e2153f235f1
AbullahUsama087 <abdullah.usama087@gmail.com>
2024-04-14 16:10:45 +0200

    Brand Routes(APIs)

---
+++ Src/Modules/Brand/brand.routes.js
@@ -0,0 +1,44 @@
+import { Router } from "express";
+
+import * as BrandControllers from "./brand.controller.js";
+
+import * as BrandValidators from "./brand.validation.js";
+import multerCloud from "../../Services/multerCloud.js";
+import allowedExtensions from "../../Utils/allowedExtensions.js";
+import { validationCoreFunction } from "../../Middlewares/validation.js";
+import { asyncHandler } from "../../Utils/errorHandling.js";
+import isAuth from "../../Middlewares/authentication.js";
+import brandApisRoles from "./brand.endpoints.js";
+
+const router = Router();
+
+router.post(
+  "/create",
+  isAuth(brandApisRoles.BRAND_ROLES),
+  multerCloud(allowedExtensions.Image).single("logo"),
+  validationCoreFunction(BrandValidators.createBrandValidation),
+  asyncHandler(BrandControllers.createdBrand)
+);
+
+router.put(
+  "/update",
+  isAuth(brandApisRoles.BRAND_ROLES),
+  multerCloud(allowedExtensions.Image).single("logo"),
+  validationCoreFunction(BrandValidators.updateBrandValidation),
+  asyncHandler(BrandControllers.updateBrand)
+);
+
+router.get(
+  "/get",
+  isAuth(brandApisRoles.BRAND_ROLES),
+  asyncHandler(BrandControllers.getAllBrands)
+);
+
+router.delete(
+  "/delete",
+  isAuth(brandApisRoles.BRAND_ROLES),
+  validationCoreFunction(BrandValidators.deleteBrandValidation),
+  asyncHandler(BrandControllers.deleteBrand)
+);
+
+export default router;
