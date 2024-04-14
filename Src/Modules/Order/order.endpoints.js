6b4cceee636bcc839f9a4761c01b35fc50d92135
AbullahUsama087 <abdullah.usama087@gmail.com>
2024-04-14 16:26:23 +0200

    Order EndPoints for Authorization

---
+++ Src/Modules/Order/order.endpoints.js
@@ -0,0 +1,8 @@
+import systemRoles from "../../Utils/systemRoles.js";
+
+const orderApisRoles = {
+  ORDER_ROLES: [systemRoles.USER],
+  DELIVER_ORDER: [systemRoles.ADMIN],
+};
+
+export default orderApisRoles;
