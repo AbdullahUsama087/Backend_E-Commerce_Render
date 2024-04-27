0ca5fdfc377b14e206e6d07e81e2ee331eab4113
AbullahUsama087 <abdullah.usama087@gmail.com>
2024-04-27 18:11:50 +0300

    add GraphQL authentication

--- Src/Middlewares/authentication.js
+++ Src/Middlewares/authentication.js
@@ -10,7 +10,7 @@ function isAuth(roles) {
         return next(new Error("Please SignIn", { cause: 400 }));
       }
 
-      // Check on Authorization Key
+      // Check on Authorization Perfix
       if (!authorization.startsWith(process.env.TOKEN_PERFIX)) {
         return next(new Error("Invalid Token Perfix", { cause: 400 }));
       }
@@ -20,18 +20,18 @@ function isAuth(roles) {
 
       try {
         // Verify Token
-        const decodeData = verifyToken({
+        const decodedData = verifyToken({
           token: splittedToken,
           signature: process.env.SIGN_IN_TOKEN,
         });
 
         // Check on Decoded Token
-        if (!decodeData || !decodeData.Id) {
+        if (!decodedData || !decodedData.Id) {
           return next(new Error("Invalid Token", { cause: 400 }));
         }
         // Check on User SignUp
         const findUser = await userModel.findById(
-          decodeData.Id,
+          decodedData.Id,
           "email userName role"
         );
         if (!findUser) {
@@ -46,7 +46,7 @@ function isAuth(roles) {
         req.authUser = findUser;
         next();
       } catch (error) {
-        // Check of Expiration Token
+        // Check on Expiration Token
         if (error == "TokenExpiredError: jwt expired") {
           // Refresh token
           const user = await userModel.findOne({ token: splittedToken });
@@ -63,7 +63,7 @@ function isAuth(roles) {
           if (!userToken) {
             return next(new Error("Fail to generate token", { cause: 400 }));
           }
-          // Update the refreshed token in database
+          // Update Refreshed Token in database
           await userModel.findOneAndUpdate(
             { token: splittedToken },
             { token: userToken },
@@ -78,9 +78,90 @@ function isAuth(roles) {
       }
     } catch (err) {
       console.error(err);
-      return next(new Error("Authentication Error", { cause: 500 }));
+      next(new Error("Authentication Error", { cause: 500 }));
     }
   };
 }
 
-export default isAuth;
+async function isAuthGraphQL(authorization, roles) {
+  try {
+    // Check Authorization
+    if (!authorization) {
+      return new Error("Please SignIn", { cause: 400 });
+    }
+    // Check on Authorization Perfix
+    if (!authorization.startsWith(process.env.TOKEN_PERFIX)) {
+      return new Error("Invalid Token Perfix", { cause: 400 });
+    }
+
+    // Split Token
+    const splittedToken = authorization.split(" ")[1];
+
+    try {
+      // Verify token
+      const decodedData = verifyToken({
+        token: splittedToken,
+        signature: process.env.SIGN_IN_TOKEN,
+      });
+
+      // Check on Decoded Token
+      if (!decodedData || !decodedData.Id) {
+        return new Error("Invalid Token", { cause: 400 });
+      }
+
+      const findUser = await userModel.findById(
+        decodedData.Id,
+        "email userName role"
+      );
+      if (!findUser) {
+        return new Error("Please SignUp", { cause: 400 });
+      }
+
+      // Check on Authorization Condition
+      if (!roles.includes(findUser.role)) {
+        return new Error("UnAuthorized User", { cause: 401 });
+      }
+
+      return {
+        code: 200,
+        findUser,
+      };
+    } catch (error) {
+      // Check on Expiration Token
+      if (error == "TokenExpiredError: jwt expired") {
+        // Refresh Token
+        const user = await userModel.findOne({ token: splittedToken });
+        if (!user) {
+          return new Error("Fail Expiration Token", { cause: 400 });
+        }
+
+        // Generate new Token
+        const userToken = generateToken({
+          payload: { email: user.email, Id: user._id },
+          signature: process.env.SIGN_IN_TOKEN,
+          expiresIn: "1h",
+        });
+        if (!userToken) {
+          return new Error("Fail to Generate Token", { cause: 400 });
+        }
+
+        // Update Refreshed Token in Database
+        await userModel.findOneAndUpdate(
+          { token: splittedToken },
+          { token: userToken },
+          { new: true }
+        );
+
+        return res
+          .status(200)
+          .json({ message: "Token Refreshed Successfully", userToken });
+      }
+      new Error("Wrong Token", { cause: 500 });
+    }
+  } catch (err) {
+    console.error(err);
+    new Error("Authentication Error", { cause: 500 });
+  }
+}
+
+export { isAuth, isAuthGraphQL };
